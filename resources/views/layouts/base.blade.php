10bf81e896ad1eea17b3f4561265705a1a9259d0
Deyand03 <defryyandy7@gmail.com>
2025-05-29 02:00:36 +0700

    menambahkan file style.css, mengganti font body dari layout.base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,10 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/main.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 </head>
 
-<body>
+<body style="font-family: Poppins">
     <div class="navbar bg-base-100 shadow-md px-8">
         <div class="navbar-start">
             <div class="dropdown">
@@ -39,12 +44,11 @@
             </ul>
         </div>
         <div class="navbar-end gap-3">
-            <a class="btn btn-primary">Masuk</a>
-            <a class="btn btn-info">Register</a>
+            <a class="btn btn-primary rounded-3xl p-5">Masuk</a>
+            <a class="btn btn-neutral rounded-3xl p-5">Register</a>
         </div>
     </div>
     @yield('content')
-    <script src="resources/js/main.js"></script>
 </body>
 
 </html>
